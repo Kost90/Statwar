575e9ad188a24355439e603e3b73758e4054e097
kost90 <kostdonc90@gmail.com>
2023-07-18 18:15:49 +0300

    added dropdown menu

--- index.js
+++ index.js
@@ -1,58 +1,77 @@
-const wrapper = document.querySelector('#wrapper_header')
-const hamburgerOpenBtn = document.querySelector('#hamburder_menu_open_btn');
-const div = document.createElement('div');
-const p = document.createElement('p');
-const statisticContainer = document.querySelector('statistic_container');
-const sideMenu = document.querySelector('#side_menu');
-const canvas = document.querySelector('#ellips');
-const defaultValue = 'Soldier';
+const wrapper = document.querySelector("#wrapper_header");
+const hamburgerOpenBtn = document.querySelector("#hamburder_menu_open_btn");
+const div = document.createElement("div");
+const p = document.createElement("p");
+const statisticContainer = document.querySelector("statistic_container");
+const sideMenu = document.querySelector("#side_menu");
+const canvas = document.querySelector("#ellips");
+const defaultValue = "Soldier";
 let currentValue = defaultValue;
+const btnArrow = document.querySelector("#btn_arrow");
 
-function OpenHamburgermenu (){
-    if(div.classList.contains('menu')){
-        p.remove();
-        div.classList.remove('menu');
-        div.remove()
-        hamburgerOpenBtn.classList.toggle('hamburder_menu_close_btn');
-        document.body.classList.toggle('body_hamburger_open');
-    }else{
-        div.classList.add('menu');
-        p.textContent = 'Упс, це меню ще очікує наповнення.'
-        div.appendChild(p);
-        wrapper.appendChild(div);
-        hamburgerOpenBtn.classList.toggle('hamburder_menu_close_btn');
-        document.body.classList.toggle('body_hamburger_open');
-    }
-};
-
-hamburgerOpenBtn.addEventListener('click', OpenHamburgermenu)
-
-function handelClick (e){
-const value = e.currentTarget.getAttribute('data-value');
-if(value === currentValue){
-    return;
-}
-if(value){
-    currentValue = value;
-    drowContent(value);
-} else{
-    // throw
+function OpenHamburgermenu() {
+  if (div.classList.contains("menu")) {
+    p.remove();
+    div.classList.remove("menu");
+    div.remove();
+    hamburgerOpenBtn.classList.toggle("hamburder_menu_close_btn");
+    document.body.classList.toggle("body_hamburger_open");
+  } else {
+    div.classList.add("menu");
+    p.textContent = "Упс, це меню ще очікує наповнення.";
+    div.appendChild(p);
+    wrapper.appendChild(div);
+    hamburgerOpenBtn.classList.toggle("hamburder_menu_close_btn");
+    document.body.classList.toggle("body_hamburger_open");
+  }
 }
 
-}
+hamburgerOpenBtn.addEventListener("click", OpenHamburgermenu);
+
+// function handelClick (e){
+// const value = e.currentTarget.getAttribute('data-value');
+// if(value === currentValue){
+//     return;
+// }
+// if(value){
+//     currentValue = value;
+//     drowContent(value);
+// } else{
+//     // throw
+// }
+
+// }
+
+// for (const child of sideMenu.children) {
+//     console.log(child.tagName);
+//     child.addEventListener('click', getLi)
+//   }
+
+//   function drowContent(value){
+//     if (canvas.getContext) {
+//         const ctx = canvas.getContext("2d");
+//         // drawing code here
+//       } else {
+//         // canvas-unsupported code here
+//       }
+//   }
+
+const parentTag = [];
 
 for (const child of sideMenu.children) {
-    console.log(child.tagName);
-    child.addEventListener('click', getLi)
-  }
+  const Li = child;
+  parentTag.push(Li);
+}
 
-  function drowContent(value){
-    if (canvas.getContext) {
-        const ctx = canvas.getContext("2d");
-        // drawing code here
-      } else {
-        // canvas-unsupported code here
-      }
-  }
+function showMenuLi() {
+  parentTag.forEach((element) => {
+    element.classList.toggle("show");
+  });
+  sideMenu.classList.toggle('direction_column');
+  sideMenu.classList.toggle('direction_row');
+  btnArrow.classList.toggle('btn_transform');
+}
 
-  drowContent(defaultValue);
+//   drowContent(defaultValue);
+
+btnArrow.addEventListener("click", showMenuLi);
