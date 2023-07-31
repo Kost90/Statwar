2075a6dbdd0c82501b61b448384bec7ee7bf9d81
kost90 <kostdonc90@gmail.com>
2023-07-31 14:45:07 +0100

    edited icons, edited PopUp window

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const wrapper = document.querySelector("#wrapper_header");
 const hamburgerOpenBtn = document.querySelector("#hamburder_menu_open_btn");
 const div = document.createElement("div");
 const p = document.createElement("p");
-const statisticContainerYear = document.querySelector("#year-statistic");
-const statisticContainerMonth = document.querySelector("#statistic_month");
-const statisticContainerWeek = document.querySelector("#statistic_week");
+const statisticContainerYear = document.querySelector("#static_ellips_year");
+const statisticContainerMonth = document.querySelector("#static_ellips_month");
+const statisticContainerWeek = document.querySelector("#static_ellips_week");
 const sideMenu = document.querySelector("#side_menu");
 const sideMenuSec2 = document.querySelector("#side_menu_sec2");
 const sideMenuSec3 = document.querySelector("#side_menu_sec3");
@@ -17,9 +17,17 @@ let currentValue = defaultValue;
 const btnArrow = document.querySelector("#btn_arrow");
 const btnArrowSec2 = document.querySelector("#btn_arrow_sec2");
 const btnArrowSec3 = document.querySelector("#btn_arrow_sec3");
+
+// ============================= PopUP=====================================
 const popUp = document.createElement("div");
 const textcontr = document.createElement("div");
 const PopUpH = document.createElement("h2");
+const closeImg = document.createElement("img");
+closeImg.setAttribute('src', './styles/images/Close_hamburger_menu_btn.png')
+const closePopupBtn = document.createElement("button");
+closePopupBtn.setAttribute('type','button');
+const popUpDivContainer = document.createElement("div");
+popUpDivContainer.classList.add('flexPopup')
 PopUpH.textContent = "Загальна кількість";
 const PopupText1 = document.createElement("p");
 const PopupText2 = document.createElement("p");
@@ -28,42 +36,30 @@ PopupText1.textContent =
   `*Дані загальної кількості військових засобів рф взяті з відкритих джерел “назва” та включають в себе: законсервовані на складах, включно на озброєнні, тощо.`;
 PopupText2.textContent = '**Дані кількості військових засобів, які перебували на озброєнні ЗС рф на початок повномаcштабного вторгнення взято з “назва”';
 
-function OpenHamburgermenu() {
-  if (div.classList.contains("menu")) {
-    p.remove();
-    div.classList.remove("menu");
-    div.remove();
-    hamburgerOpenBtn.classList.toggle("hamburder_menu_close_btn");
-    document.body.classList.toggle("body_hamburger_open");
-  } else {
-    div.classList.add("menu");
-    p.textContent = "Упс, це меню ще очікує наповнення.";
-    div.appendChild(p);
-    wrapper.appendChild(div);
-    hamburgerOpenBtn.classList.toggle("hamburder_menu_close_btn");
-    document.body.classList.toggle("body_hamburger_open");
-  }
+function OpenPopup(e) {
+    popUp.classList.add("popup");
+    closePopupBtn.appendChild(closeImg);
+    popUpDivContainer.appendChild(PopUpH);
+    popUpDivContainer.appendChild(closePopupBtn);
+    textcontr.appendChild(PopupText1);
+    textcontr.appendChild(PopupText2);
+    popUp.appendChild(popUpDivContainer);
+    popUp.appendChild(textcontr);
+    e.appendChild(popUp);
 }
 
-hamburgerOpenBtn.addEventListener("click", OpenHamburgermenu);
-
-function OpenPopup(e) {
-  if (popUp.classList.contains("popup")) {
+closePopupBtn.addEventListener('click', (e) => {
+  e.stopPropagation();
     popUp.classList.remove("popup");
     popUp.remove();
+    popUpDivContainer.remove();
     textcontr.remove();
     PopUpH.remove();
     PopupText1.remove();
     PopupText2.remove();
-  } else {
-    popUp.classList.add("popup");
-    textcontr.appendChild(PopupText1);
-    textcontr.appendChild(PopupText2);
-    popUp.appendChild(PopUpH);
-    popUp.appendChild(textcontr);
-    e.appendChild(popUp);
-  }
-}
+    closeImg.remove();
+    closePopupBtn.remove();
+});
 
 statisticContainerMonth.addEventListener("click", (e) => {
   const element = e.currentTarget;
@@ -85,6 +81,26 @@ statisticContainerWeek.addEventListener("click", (e) => {
   console.log(closest);
   OpenPopup(closest);
 });
+// =================================================================================
+
+function OpenHamburgermenu() {
+  if (div.classList.contains("menu")) {
+    p.remove();
+    div.classList.remove("menu");
+    div.remove();
+    hamburgerOpenBtn.classList.toggle("hamburder_menu_close_btn");
+    document.body.classList.toggle("body_hamburger_open");
+  } else {
+    div.classList.add("menu");
+    p.textContent = "Упс, це меню ще очікує наповнення.";
+    div.appendChild(p);
+    wrapper.appendChild(div);
+    hamburgerOpenBtn.classList.toggle("hamburder_menu_close_btn");
+    document.body.classList.toggle("body_hamburger_open");
+  }
+}
+
+hamburgerOpenBtn.addEventListener("click", OpenHamburgermenu);
 
 
 function handelClick(e) {
